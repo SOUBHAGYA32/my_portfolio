5ddfb13f549fc8c63a9db30c3939633b102e3783
SOUBHAGYA32 <soubhagya801832@gmail.com>
2022-01-18 00:25:52 +0530

    Pages and Components Set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
+import {BrowserRouter , Routes , Route } from 'react-router-dom'
 import './App.css';
+import Layout from './Components/Layout';
+
+//Pages
+import Home from './Pages/Home';
+import Projects from './Pages/Projects';
+import Courses from './Pages/Courses';
+import Contact from './Pages/Contact';
+import About from './Pages/About';
+
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-green-500'>Hello tailwind Css</h1>
+      <BrowserRouter>
+         
+         <Routes>
+
+               <Route path='/' element={<Home/>}/>
+               <Route path='/projects' element={<Projects/>}/>
+               <Route path='/courses' element={<Courses/>}/>
+               <Route path='/contact' element={<Contact/>}/>
+               <Route path='/about' element={<About/>}/>
+
+         </Routes>
+
+     </BrowserRouter>
     </div>
   );
 }
